d2937875a160f2f944adc1d1422978e8e07b37c8
jembarhakim <158005685+jembarhakim@users.noreply.github.com>
2024-08-29 10:08:24 +0700

    perubahan

---
+++ app/src/main/java/com/example/linier_layout/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.linier_layout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button btnSubmit;
+    private EditText edtEmail;
+    private EditText edtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+        btnSubmit = findViewById(R.id.btnSubmit);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edtEmail.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                Toast.makeText(getApplicationContext(), "Email: " + email +
+                        "\nPassword: " + password, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
